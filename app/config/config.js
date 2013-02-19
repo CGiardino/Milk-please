449ab637426d7ea7d1aa8a97e327e4888bc92f51
CGiardino <carmine.giardino@gmail.com>
2013-02-19 19:53:58 +0100

    updated pinterest

---
+++ app/config/config.js
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * 
+ * Application settings
+ * 
+ ******************************************************************************/
+
+module.exports = function configure(app) {
+
+  /*----------------------------------------------------------------------------
+    Current environment
+   ---------------------------------------------------------------------------*/
+
+  app.set('env', 'development');
+
+  /*----------------------------------------------------------------------------
+    Settings for all environments
+   ---------------------------------------------------------------------------*/
+
+  //Web server port
+  app.set('port', process.env.PORT || 3000);
+
+  //Default controller, to be called on '/' requests
+  app.set('defaultController', 'index');
+
+  //Default template engine
+  app.set('engine', 'ejs');
+
+  //Default view extension
+  app.set('extension', 'html');
+
+  /*----------------------------------------------------------------------------
+    Development settings
+   ---------------------------------------------------------------------------*/
+
+  if(app.get('env') === 'development') {
+    //Add development-specific settings here
+  }
+
+  /*----------------------------------------------------------------------------
+    Production settings
+   ---------------------------------------------------------------------------*/
+
+  if(app.get('env') === 'production') {
+    //Add production-specific settings here
+  }
+}
---
+++ app/controllers/mobile/mobile.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: carminegiardino
+ * Date: 1/19/13
+ * Time: 3:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+exports.read = function() {
+    this.render();
+}
