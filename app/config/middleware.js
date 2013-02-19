d18688d63bb41f4a5adfa4359f6439d996a26ca0
CGiardino <carmine.giardino@gmail.com>
2013-02-19 18:50:05 +0100

    updated presskit

--- app/config/middleware.js
+++ app/config/middleware.js
@@ -17,7 +17,7 @@ module.exports = function addMiddleware(app) {
   app.use(express.limit('5mb'));
   
   //Favicon
-  app.use(express.favicon(path.join(app.get('appDir'), 'public' , 'favicon.png')));
+  app.use(express.favicon(path.join(app.get('appDir'), 'public/img/favicon' , 'favicon.png')));
   
   //Logger
   app.use(express.logger({format: 'dev'}));
